46d8551a62fdc60b11fa2d04937aa2514f2acf47
Phikolomzi Gugwana <ggwphi001@myuct.ac.za>
2022-09-03 20:58:39 +0200

    Feature/modify for pypi (#3)
    * added license and setup
    
    * updated setup.py
    
    * updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'A package to help developers build front end user interfaces for machine learning models.'
 LONG_DESCRIPTION = 'A package to help developers build front end user interfaces for machine learning models.'
 
@@ -22,7 +22,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=[],
-    keywords=['python', 'django'],
+    keywords=['python', 'django', 'machine learning', 'user interface'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
