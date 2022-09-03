90557b1a76efc9a7c6407ec3e3d3013b3347a411
Phikolomzi Gugwana <ggwphi001@myuct.ac.za>
2022-09-03 20:51:29 +0200

    added license and setup (#2)

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package to help developers build front end user interfaces for machine learning models.'
+LONG_DESCRIPTION = 'A package to help developers build front end user interfaces for machine learning models.'
+
+# Setting up
+setup(
+    name="django-machine-learning-user-interface",
+    version=VERSION,
+    author="Phikolomzi Gugwana",
+    author_email="<ggwphi001@myuct.ac.za>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'django'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
